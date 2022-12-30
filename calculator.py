e95069f8c326ca775a48792f82b8802fbffad054
DjordjeG <djordjeguduric@gmail.com>
2022-12-30 11:14:51 +0100

    Fajl

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# a = int(input("Frist Number: ",  ))
+# b= int(input("Second Number",  ))
+# print("result is", a+b)
+
+# eru = 1.3
+# usd = 1.1 
+# val = float(input("Enter value:", ))
+# total_euro = "Total_euro: %.2f" % (val/eru)
+# total_usd = "Total_usd:%.2f" % (val/usd)
+# print(total_euro)
+# print(total_usd)\
+
+# price = float(input("Enter price",  ))
+# tax = 0.2
+# price_wit_tax = price*tax + price
+# print("value with tax is:", price_wit_tax)
+r = float(input("Enter radius"))
+
+pi = 3.14
+P = r**2 * pi
+print("circle area is", P)
